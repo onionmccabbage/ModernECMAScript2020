79e1862c318b0875759270d0ef34ab0bc2f86cee
Lisa <onionmccabbage@live.com>
2020-10-27 13:30:25 +0000

    day 2a

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+// import our custom components
+import About from './components/about'
+
+function App() {
+  // declare properties of this component
+  const products = [{id:0, 'name':'dots', 'price':'2.99'},
+                      {id:1, 'name':'pots', 'price':'3.99'},
+                      {id:2, 'name':'spots', 'price':'4.99'}]
+
+  return ( // this is NOT html, it is JSX. Use lower case for html and PascalCase for our own components
+    <div className="App"> 
+      <h2>Welcome to React</h2>
+      <About title='This is a string literal' products={products} />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/about.js
@@ -0,0 +1,29 @@
+// import stuff we need
+import React from 'react'
+
+// declare a functional component
+const About = ( {title, products} )=>{ // we an choose to pass in props, here called 'title'
+    // set up any properties
+    
+    // .. and any methods
+    // the ONE thing we MUST have is a return statement
+    return (
+        <section>
+            <h4>This is the About screen</h4>
+            <p>it is a functional component</p>
+            <p>{title}</p>
+            <ul>
+                {/* this is a JSX comment */}
+                {/* we can iterate over our products data model */}
+                {products.map( (el)=>{
+                    return (<li key={el.id}>{el.name} &pound;{el.price}</li>)
+                } )}
+                {/* or */}
+                {/* {products.map( el=> (<li key={el.id}>{el.name} &pound;{el.price}</li>) ) } */}
+            </ul>
+        </section>
+    )
+}
+
+// make this component available by exporting it
+export default About

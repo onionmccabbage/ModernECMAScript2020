50e57296d9aa389130a4942733b1859c7766c25d
Lisa <onionmccabbage@live.com>
2020-10-28 09:16:17 +0000

    day 3

---
+++ my-ts-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// this is a typescript file
+// we can use as much or as little ts as we like
+function App() {
+  // we can declare what types we expect to use
+
+
+  return (
+    <div className="App">
+      <h2>dummy text</h2>
+    </div>
+  )
+}
+
+
+export default App;
